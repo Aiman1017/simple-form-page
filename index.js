68d50f3cd7ac91afc9a3bb437f11d6aada4639c6
Aiman Irfan <aimanirfan1017@gmail.com>
2020-09-15 14:48:04 +0800

    need to fix the formtable function later

---
+++ index.js
@@ -0,0 +1,25 @@
+let information = []
+
+function sentform(){
+    let completeInfo = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value
+    }    
+    information.push(completeInfo);
+    formtable();
+}
+
+function formtable(e){
+    let table = document.getElementById('listTable');
+    let tablerow = document.createElement('tr')
+    let data = document.createElement('td')
+    let data2 = document.createElement('td')
+    for(i = 0; i < information.length; i++){
+        data.innerHTML = information[i].name
+        data2.innerHTML = information[i].email
+
+        tablerow.append(data);
+        tablerow.append(data2)
+        table.append(tablerow);
+    }
+}
